900c93bfcb80baa6215833ba5b331712f7044cb5
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-02-22 11:40:19 -0600

    fresh start local storage

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import './App.css';
+
+
+const baseUrl = 'http://localhost:3000/'
+const loginUrl = baseUrl + 'login'
+
+function App() {
+  useEffect( () => {
+    if (localStorage.vid)
+      console.log('User found:', localStorage.vid)
+    else
+      console.log('No user found')
+  }, [])
+
+  fetch( loginUrl, {
+    method: 'POST', 
+    headers: {
+      'content-type': 'application/json',
+      accept: 'application/json'
+    }, 
+    body: JSON.stringify({
+      email: 'sam@sam.com',
+      password: '1234Test_'
+    })
+  })
+    .then( r => r.json())
+    .then( viewer => localStorage.vid = viewer.vid)
+
+
+  return (
+    <div className="App">
+      <h2>App</h2>
+    </div>
+  );
+}
+
+export default App;
